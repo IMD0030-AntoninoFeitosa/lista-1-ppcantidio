98fcedddf9085dec80ca18b4e90250579606e1be
pedro <ppcantidio@gmail.com>
2022-04-05 17:43:31 -0300

    atividade negativos5

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -4,9 +4,18 @@ using std::cout;
 
 const int SIZE = 5; // input size.
 
-int main(void)
-{
-    // TODO: Adicione aqui seu código.
+int main(void){
+    int cont = 0;
+    int numero;
+
+    for(int i = 0; i < 5; i++){
+        cin >> numero;
+        if (numero < 0){
+            cont++;
+        }
+    }
+
+    cout << cont;
 
     return 0;
 }
