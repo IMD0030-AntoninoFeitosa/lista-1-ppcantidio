d3b42785e86cc4794aa3bc602e389585e30bce83
Pedro Cantidio <ppcantidio@gmail.com>
2022-04-09 12:09:05 -0300

    intervalos

--- intervalos/src/main.cpp
+++ intervalos/src/main.cpp
@@ -11,11 +11,25 @@ using std::endl;
 #include <iomanip>
 using std::setprecision;
 
-// Se desejar, crie funções aqui, antes do main().
 
 int main(void)
 {
-    // TODO: Adicione aqui a sua solução.
+    int SIZE = 5;
+    int count[5];
+    int total = 0;
+    int x;
+
+    while( cin >> std::ws >> x) {
+        if(x >= 0 && x < 25) count[0]++;
+        else if(x >= 25 && x < 50) count[1]++;
+        else if(x >= 50 && x < 75) count[2]++;
+        else if(x >= 75 && x < 100) count[3]++;
+        else if(x < 0 || x >= 100) count[4]++;
+    }
+
+    for(int i = 0; i < SIZE; i++) {
+        std::cout << std::setprecision(4) << ((float)count[i]/(float)total)*100 << std::endl;
+    }
 
     return 0;
 }
