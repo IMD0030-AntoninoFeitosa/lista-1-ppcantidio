48adcaed766e787d79fddd9b38890b6724ee2a74
pedro <ppcantidio@gmail.com>
2022-04-11 19:23:46 -0300

    ajustes gerais no codigo

--- soma_vizinhos/src/main.cpp
+++ soma_vizinhos/src/main.cpp
@@ -10,31 +10,28 @@ using std::endl;
 
 int main( void )
 {
-    // 0 was set as initialization value to avoid undefined behavior
-    int m = 0;    // holds the starting number
-    int n = 0;    // holds the number of consecutive integers and the counting direction
+    int m = 0;    
+    int n = 0;   
     int sum = 0;  
     int startingNumber = 0;
 
     while(std::cin >> m >> n) {
-        // resets the variables
         startingNumber = m;
         sum = 0;
 
-        if(n > 0) {    // increasing
+        if(n > 0) {  
             for(int count = 0; count < n; count++) {
                 sum += startingNumber;
                 startingNumber++;
             }
-        } else if(n < 0) { // decreasing
-            int absn = n * (-1); // absolute value of n
+        } else if(n < 0) {
+            int absn = n * (-1); 
             for(int count = 0; count < absn; count++) {
                 sum += startingNumber;
                 startingNumber--;
             }
-        } else sum = m; // zero step
+        } else sum = m;
 
-        // output
         std::cout << sum << std::endl;
     }
 
